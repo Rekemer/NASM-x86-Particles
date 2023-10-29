47e21d4fdcf3b2c83984bd3f9d83607ee98c8e0f
Rekemer <ilya.barselona@gmail.com>
2023-10-29 16:41:16 +0300

    create window

--- hello.asm
+++ hello.asm
@@ -6,7 +6,15 @@
 ; ----------------------------------------------------------------------------
 
 
+; Remember that labels can be used to refer to data in code. There are two
+; ways that a label can be used. If a plain label is used, it is interpreted as the
+; address (or offset) of the data. If the label is placed inside square brackets
+; ([]), it is interpreted as the data at the address. In other words, one should
+; think of a label as a pointer to the data and the square brackets dereferences
+; the pointer just as the asterisk does in C.
 
+%include "WIN32N.INC"
+                             
     COLOR_WINDOW        EQU 5                       ; Constants
     CS_BYTEALIGNWINDOW  EQU 2000h
     CS_HREDRAW          EQU 2
@@ -33,6 +41,8 @@
     extern LoadImageA
     extern PostQuitMessage
     extern RegisterClassExA
+    extern LoadIconA
+    extern LoadCursorA
     extern ShowWindow
     extern TranslateMessage
     extern UpdateWindow
@@ -60,12 +70,71 @@ _main:
     sub esp, 32
     xor ecx,ecx
     call [GetModuleHandleA]
-    ;mov [REL hInstance], eax
+    mov [hInstance], eax
     add esp, 32
-    add esp, 12
-    push 3
-    call [ExitProcess]
+    add esp, 8
+
+    
+    
+    mov dword[OurWindowclass], 48 ; .cbSize
+    mov dword[OurWindowclass+4], CS_OWNDC|CS_HREDRAW|CS_VREDRAW 
+    mov dword[OurWindowclass+8], WindowProc    ; lpfnWndProc
+    mov dword[OurWindowclass+12], 0
+    mov dword[OurWindowclass+16], 0
+    mov dword eax, [hInstance]
+    mov dword[OurWindowclass+20], eax ; .hInstance
+
+    push IDI_APPLICATION
+    push 0
+    call LoadIconA
+	mov dword[OurWindowclass+24], eax ; .hIcon
+    
+    push IDI_APPLICATION
+    push 0
+    call LoadCursorA
+    mov dword[OurWindowclass+28], eax ; .hCursor
+
+    mov dword[OurWindowclass+32], 0 ; .hbrBackground, 5 is COLOR_WINDOW
+    mov dword[OurWindowclass+36], 5 ; .lpszMenuName
+    mov dword[OurWindowclass+40], "a" ; .lpszClassName
+    mov dword[OurWindowclass+44], 0 ; .lpszClassName
+    lea eax, [OurWindowclass]
+    push eax
+    call RegisterClassExA
+    add esp, 4
+   
+    ;mov eax, dword[Instance]
+
+    push 0
+    push dword[hInstance]
+    push 0
+    push 0
+    push 362    ;  Height
+    push 362    ;  Width
+    push 140    ;  Top
+    push 360    ;  Left
+    push WS_VISIBLE+WS_OVERLAPPEDWINDOW ; Style
+    push WindowName
+    push ClassName
+    push 0        ; Extended style
+    call CreateWindowExA
+    mov dword[Windowhandle], eax
+    
+    
+    push 69
+    call ExitProcess 
+
     ret
+CreateWindow: 
+    
+WindowProc:
+    push ebp
+    mov ebp, esp
+
+    mov esp, ebp
+    pop ebp
+    ret
+    
 section .data  ; initialized and constant data 
     number: dd 22
     number2: dd 5
@@ -80,4 +149,7 @@ section .data  ; initialized and constant data
 message:
     db  '%i', 10, 0
 section .bss
-     hInstance resb 1
+     alignb 8
+     hInstance resb 4
+     OurWindowclass resb 48
+     Windowhandle resb 4
